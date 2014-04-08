3f2cdae5e52a6f28a17d8775728632024f3639dd
keval <keval@kalrav>
2014-04-08 22:24:16 +0530

    add email_directory

---
+++ apps/backend/modules/email_directory/lib/email_directoryGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * email_directory module configuration.
+ *
+ * @package    BestBuddies
+ * @subpackage email_directory
+ * @author     Anvaya Technologies
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class email_directoryGeneratorConfiguration extends BaseEmail_directoryGeneratorConfiguration
+{
+}
