e2e0077b7217a4a15bb8c6f223afd414c036c8e0
keval <keval@kalrav>
2014-04-11 22:22:40 +0530

    add member list pdf action

--- lib/form/email_directoryCollectionForm.class.php
+++ lib/form/email_directoryCollectionForm.class.php
@@ -10,15 +10,34 @@ class email_directoryCollectionForm extends sfForm
     {
       $this->widgetSchema['name'] = new sfWidgetFormInputText();
       $this->setDefault('name', $this->getOption('name'));
+      $this->validatorSchema['name'] = new sfValidatorPass();
       $this->widgetSchema['email'] = new sfWidgetFormInputText();
       $this->setDefault('email', $this->getOption('email'));
+      $this->validatorSchema['email'] = new sfValidatorEmail(array('required'=>FALSE));
       
-      $this->mergePostValidator();
+      //$this->mergePostValidator();
+      // add a post validator
+      $this->validatorSchema->setPostValidator(
+        new sfValidatorCallback(array('callback' => array($this, 'checkNameEmail')))
+      );
     }
     
     public function addDetailRow($key)
     {
     }
+    
+    public function checkNameEmail($validator, $values)
+    {
+      if((trim($values['name']) != "" && trim($values['email']) == "") || (trim($values['name']) == "" && trim($values['email']) != ""))
+      {
+        // value is not correct, throw an error
+        throw new sfValidatorError($validator, 'Add Email and Name');
+      }
+
+      // value is correct, return the clean values
+      return $values;
+    }
+      
 }
 
 ?>
